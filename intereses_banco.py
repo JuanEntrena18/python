6c7735f052078b4835afff29ba031f93bd5c3067
Juan Entrena <jentrenagarr@protonmail.com>
2025-09-26 14:44:55 +0200

    Prueba interes

---
+++ intereses_banco.py
@@ -0,0 +1,8 @@
+cantidad=float(input('Introduce el capital inicial a invertir: '))
+interes=float(input('Introduce el interés, sólo número:'))
+tiempo=float('¿Cuantos años vas a tener el dinero en el banco? ')
+
+interesTotal=round(cantidad*((1+interes/100),tiempo),2)
+
+print("El capital a obtener será de: "+str(interesTotal))                  
+1
