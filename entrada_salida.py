6b752bfdef7b1ba49aa60b2c2ed85549ffa4a6b5
Juan Entrena <jentrenagarr@protonmail.com>
2025-09-26 13:58:31 +0200

    Subida Entrada salida

---
+++ entrada_salida.py
@@ -0,0 +1,6 @@
+base=float(input("Introduce el número de la base "))
+altura=float(input("Introduce el número de la altura "))
+area=base*altura
+perimetro=2*base+2*altura
+print ("El área es",area)
+print ("el perímetro es ", perimetro)
