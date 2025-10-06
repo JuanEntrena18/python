40c01316381fce4df22f9ad600329ad28b96815f
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-06 12:54:24 +0200

    Terminado

--- ejercicios/ejercicio_9.py
+++ ejercicios/ejercicio_9.py
@@ -2,7 +2,7 @@
 #Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual 
 #y el número de años, y muestre por pantalla el capital obtenido en la inversión. 
 cantidad=float(input("Introduce el capital inicial a invertir: "))
-interes=float(input("Introduce el interés, sólo número:"))
-tiempo=float(input("¿Cuantos años vas a tener el dinero en el banco?"))
-interesTotal=round(cantidad*((1+interes/100),tiempo),2)
-print("El capital a obtener será de: ", +str(interesTotal))
+interes=float(input("Introduce el interés, sólo número: "))
+tiempo=float(input("¿Cuantos años vas a tener el dinero en el banco? "))
+interesTotal=round (cantidad * (1 + interes / 100) ** tiempo, 2)
+print("El capital a obtener será de: ", interesTotal)
