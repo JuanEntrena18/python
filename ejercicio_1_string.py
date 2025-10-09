8542ebd5f8e126628ae876e469e742508f2625db
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-09 10:50:23 +0200

    Empezamos con los str

---
+++ ejercicio_1_string.py
@@ -0,0 +1,7 @@
+#Ejercicio 1
+#Escribir un programa que pregunte el nombre del usuario en la consola y un número
+#entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces
+#como el número introducido.
+nombre=str(input("¿Cuál es tu nombre de usuario "))
+multi=int(input("¿Cuántas veces quieres que se repita el nombre? "))
+print (nombre*multi, " ")
