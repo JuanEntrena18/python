8b3ce247654d2e9ddde097d7578c7445f97440f7
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-09 11:05:26 +0200

    Añadidas interrogaciones

--- ejercicios/ejercicio_2_string.py
+++ ejercicios/ejercicio_2_string.py
@@ -4,9 +4,9 @@
 #todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la
 #primera letra del nombre y de los apellidos en mayúscula. El usuario puede
 #introducir su nombre combinando mayúsculas y minúsculas como quiera.
-nombre=str(input("¿Cuál es tu nombre "))
-apellido1=str(input("¿Cuál es tu primer apellido "))
-apellido2=str(input("¿Cuál es tu segundo apellido "))
+nombre=str(input("¿Cuál es tu nombre? "))
+apellido1=str(input("¿Cuál es tu primer apellido? "))
+apellido2=str(input("¿Cuál es tu segundo apellido? "))
 print ("Tu nombre completo es ", nombre.lower() +"", apellido1.lower() +"", apellido2.lower())
 print ("Tu nombre completo es ", nombre.upper() +"", apellido1.upper() +"", apellido2.upper())
 print ("Tu nombre completo es ", nombre.title() +"", apellido1.title() +"", apellido2.title())
