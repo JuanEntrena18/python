1eb56651839d5184afe7ceb3262844e470cd884e
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-16 09:57:30 +0200

    Ejercicio 5 completo

---
+++ ejercicios/ejercicio_5_string.py
@@ -0,0 +1,6 @@
+#Ejercicio 5 
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y 
+#muestre por pantalla la frase invertida.
+frase=str(input("Introduce una frase en la pantalla y yo la invertiré: "))
+invertir=frase[::-1]
+print ("La frase original es ", frase, "y la frase invertida es", invertir)
