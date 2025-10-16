5cea24b0902fbc875f90881bb19ad8119e8cb1df
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-16 10:46:29 +0200

    Ejercicio 7 completo

--- ejercicios/ejercicio_7_string.py
+++ ejercicios/ejercicio_7_string.py
@@ -3,4 +3,5 @@
 #muestre por pantalla otro correo electrónico con el mismo nombre (la parte delante 
 #de la arroba @) pero con dominio ceu.es
 correo=str(input("Introduzca su correo electrónico: "))
-
+eliminar=correo.split("@")[0]
+print ("Tu nuevo correo es ",eliminar+"@ceu.es")
