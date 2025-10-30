c63219e44212bd6c925ad3209bf9c5f48170ef70
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-30 10:42:08 +0100

    ejercicio 1 bucles

---
+++ ejercicios/Bucles/ejercicio_1_bucles.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10
+#veces.
+palabra = str(input("Dime una palabra y te la multiplico por 10 "))
+contador = 0
+while contador <10:
+    print (palabra)
+    contador = contador +1
+
+
