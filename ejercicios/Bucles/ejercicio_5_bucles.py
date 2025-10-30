65c0584d3156bd66208eb969d08c3ebd0caeb9d6
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-30 11:15:20 +0100

    Ejercicio 5 sin terminar

---
+++ ejercicios/Bucles/ejercicio_5_bucles.py
@@ -0,0 +1,5 @@
+#Ejercicio 5 
+#Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual 
+#y el número de años, y muestre por pantalla el capital obtenido en la inversión cada 
+#año que dura la inversión
+cantidad = float(input("Dime la cantidad que quieres invertir"))
