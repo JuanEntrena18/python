bdd9499365e7990cb4a9f8576a3abdc2e038f6fb
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-30 10:07:57 +0100

    terminado ejercicio 9 condic

---
+++ ejercicios/Condicionales/ejercicio_9_condicionales.py
@@ -0,0 +1,15 @@
+#Ejercicio 9 
+#Escribir un programa para una empresa que tiene salas de juegos para todas las 
+#edades y quiere calcular de forma automática el precio que debe cobrar a sus 
+#clientes por entrar. El programa debe preguntar al usuario la edad del cliente y 
+#mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis, 
+#si tiene entre 4 y 18 años debe pagar 5€ y si es mayor de 18 años, 10€. 
+edad_cliente = int(input("¿Qué edad tienes, chaval? "))
+if (edad_cliente <= 4):
+    print ("Has tenido suerte, entras gratis")
+elif (edad_cliente <=18):
+    print ("Lo siento, te toca pagar 5 euros")
+elif (edad_cliente >18):
+    print ("Como adulto, te toca pagar el máximo, 10 euros")
+else:
+    print ("Te has equivocado de edad, chaval")
