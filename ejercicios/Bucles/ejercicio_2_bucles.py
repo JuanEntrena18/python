0363bb4cf9e0d75e347b194af5462ed8827db8a9
Juan Entrena <jentrenagarr@protonmail.com>
2025-10-30 10:48:36 +0100

    Ejercicio 2 terminado

---
+++ ejercicios/Bucles/ejercicio_2_bucles.py
@@ -0,0 +1,8 @@
+#Ejercicio 2 
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos 
+#los años que ha cumplido (desde 1 hasta su edad).
+edad = int(input("Dime tu edad"))
+contador = 1
+while contador <= edad:
+    print (contador)
+    contador=contador+1
