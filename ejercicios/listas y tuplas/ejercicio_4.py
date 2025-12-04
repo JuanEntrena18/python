a6328623560465485b6ffba05a97aa651806653b
Juan Entrena <jentrenagarr@protonmail.com>
2025-12-04 10:46:34 +0100

    Ejercicio 4 completado correcto

--- ejercicios/listas y tuplas/ejercicio_4.py
+++ ejercicios/listas y tuplas/ejercicio_4.py
@@ -2,6 +2,9 @@
 #Escribir un programa que pregunte al usuario los números ganadores de la lotería 
 #primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor 
 #a mayor.
-ganadores = [2,45,97,56,32,1,9]
-ganadores.sort()
-print(ganadores)
+num_ganadores = []
+for i in range(6):
+    temp = int(input("Dime los 6 números ganadores " + str(i+1) + ": "))
+    num_ganadores.append(temp)
+num_ganadores.sort()
+print ("Los números ganadores son: ", *num_ganadores)
