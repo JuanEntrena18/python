a094987239c0689c03e3f1d184a9f868d85a24af
Juan Entrena <jentrenagarr@protonmail.com>
2025-12-04 10:00:22 +0100

    ejercicio 4 terminado

--- ejercicios/listas y tuplas/ejercicio_4.py
+++ ejercicios/listas y tuplas/ejercicio_4.py
@@ -2,3 +2,6 @@
 #Escribir un programa que pregunte al usuario los números ganadores de la lotería 
 #primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor 
 #a mayor.
+ganadores = [2,45,97,56,32,1,9]
+ganadores.sort()
+print(ganadores)
