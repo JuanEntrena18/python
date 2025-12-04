421d6f682b1bf2820600e3276f24aed787c896f3
Juan Entrena <jentrenagarr@protonmail.com>
2025-12-04 10:55:14 +0100

    ejercicio 7 terminado

--- ejercicios/listas y tuplas/ejercicio_7.py
+++ ejercicios/listas y tuplas/ejercicio_7.py
@@ -3,3 +3,6 @@
 #letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista 
 #resultante. 
 abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+## uso de enumerate de la lista abc para que empiece en el 1
+resultado = [letra for i, letra in enumerate(abc, start=1) if i % 3 != 0]
+print("El resultado es", *resultado)
