2fdacc835e8100961180d27306e821f893d8d7d0
Juan Entrena <jentrenagarr@protonmail.com>
2025-12-04 10:14:07 +0100

    Ejercicio 5 terminado

---
+++ ejercicios/listas y tuplas/ejercicio_5.py
@@ -0,0 +1,10 @@
+#Ejercicio 5 
+#Escribir un programa que almacene en una lista los números del 1 al 10 y los 
+#muestre por pantalla en orden inverso separados por comas.
+numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros.reverse()
+## print (* variable) elimina las comas
+print(*numeros)
+## usando replace ponemos las comas donde haga falta
+print (str(numeros).replace('[','').replace(']',''))
+
