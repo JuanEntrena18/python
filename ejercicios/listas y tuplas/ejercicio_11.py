f3e34fa454b1089ec459455d81a1ded3871e7ab2
Juan Entrena <jentrenagarr@protonmail.com>
2026-01-08 10:54:41 +0100

    Ejercicio 11

---
+++ ejercicios/listas y tuplas/ejercicio_11.py
@@ -0,0 +1,7 @@
+#Ejercicio 11 
+#Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y 
+#muestre por pantalla su producto escalar.
+vectorA = [1,2,3]
+vectorB = [-1,0,2]
+resultado = (vectorA[0] * vectorB[0] + vectorA[1] * vectorB[1] + vectorA[2] * vectorB[2])
+print ("El resultado es ",resultado) 
