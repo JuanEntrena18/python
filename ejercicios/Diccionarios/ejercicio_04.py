05585c8f72cb828a60a1e76f9f9c845ed54813c5
Juan Entrena <jentrenagarr@protonmail.com>
2026-01-09 14:43:52 +0100

    Ejercicio 4, sin terminar

---
+++ ejercicios/Diccionarios/ejercicio_04.py
@@ -0,0 +1,19 @@
+#Ejercicio 4
+#Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre
+#por pantalla la misma fecha en formato dd de <mes> de aaaa donde <mes> es
+#el nombre del mes
+
+calendario = {
+    "enero": 01,
+    "febrero": 02,
+    "marzo": 03,
+    "abril": 04,
+    "mayo": 05,
+    "junio": 06,
+    "julio": 07,
+    "agosto": 08,
+    "septiembre": 09,
+    "octubre": 10,
+    "noviembre": 11,
+    "diciembre": 12
+}
